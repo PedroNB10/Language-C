4618cc72392d8f6291fcaa66cd8b0bdea8ae1fbb
Pedro Nogueira <pedro.nogueira.barboza@gmail.com>
2023-04-16 17:51:13 -0300

    adicionei os projetos

---
+++ Aula04/caracteres.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+
+
+int main ()
+{
+
+    char O = 79; //numeracao decimal no sistema ASCII
+    char L = 76;
+    char A = 65;
+
+    char o = 'o'; //caracter especifico que deseja armazenar
+    char l = 'l';
+    char a = 'a';
+
+    printf("%c%c%c, MUNDO! \n",O,L,A);
+
+    printf("%c%c%c, mundo! \n ",o,l,a);
+
+
+    return 0;
+}
---
+++ Aula04/numerosAleatorios.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+int main () 
+{
+    int numero1;
+    int numero2;
+
+    srand( time(NULL) ); //gera uma semente a partir do horario contado, se nao tiver nao vai ser aleatorio e o valor semente sempre sera zero
+    numero1 = rand(); //gera um numero aleatorio a partir da seed 
+    numero2 = rand();
+    printf("%d \n",numero1);
+    printf("%d \n",numero2);
+
+    return 0;
+
+}
