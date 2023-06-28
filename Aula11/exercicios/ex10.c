e93c855bbcf99caffc9b215b1d56174d14ad0367
Pedro Nogueira <pedro.nogueira.barboza@gmail.com>
2023-06-27 23:41:09 -0300

    adicionei o ex10

---
+++ Aula11/exercicios/ex10.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h> // malloc
+#include <math.h>
+int main()
+{
+    int N, i, contador = 0;
+    int *indiceNumProxMedia = (int *)malloc(sizeof(int));
+    scanf(" %d", &N);
+    double *precisao = (double *)malloc((N) * sizeof(double)), menorVariacao = INFINITY;
+
+  
+    double *vet = (double *)malloc((N) * sizeof(double));
+    double soma = 0, media;
+ 
+
+    for (i = 0; i < N; i++)
+    {
+        scanf(" %lf", &vet[i]);
+        soma = soma + (vet[i]);
+    }
+    media = soma / N;
+
+    if (media == 0)
+    {
+        *indiceNumProxMedia = 0;
+    }
+    else
+    {
+
+        for (i = 0; i < N; i++)
+        {
+
+            precisao[i] = fabs(vet[i] - media);
+            if (precisao[i] < menorVariacao)
+            {
+                menorVariacao = precisao[i];
+                *indiceNumProxMedia = i;
+            }
+        }
+    }
+
+    printf("Valor mais próximo da média: %.2lf posição: vet[%d]\n", vet[*indiceNumProxMedia], *indiceNumProxMedia);
+    printf("media: %.2lf\n", media);
+    free(vet);
+    free(indiceNumProxMedia);
+
+    return 0;
+}
